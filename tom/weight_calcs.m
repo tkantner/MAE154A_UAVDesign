6b339524949bfe75f375b4c5ae506753ff772ee5
tkantner <32119277+tkantner@users.noreply.github.com>
2019-01-27 17:42:48 -0800

    Update weight_calcs.m
    -Add add'l checks for new specs
    -Fix error in Wing Weight Nicolai Eq
    -Add new Parameters
    -Few other things

--- tom/weight_calcs.m
+++ tom/weight_calcs.m
@@ -12,6 +12,7 @@ design_num = 0;
 
 %Physical constants
 rho_10k = 17.56e-4; %Atmos. Density at 10k ft [slugs/ft^3]
+rho_5k = .002048;   %Atoms. Density at 5k ft [slugs/ft^3]
 rho_sl = 23.77e-4; %Atmos. Density at sl [slugs/ft^3]
 
 %Import the engines spreadsheet, remember to sort by ascending power
@@ -19,12 +20,19 @@ engines = xlsread('Engine_Database.xlsx');
 
 %Specs
 endur = 2; %Endurance [hrs]
-RC = 3000;  %Rate of Climb [fpm]
+RC = 1500;  %Rate of Climb [fpm]
 RC = RC/60; %Rate of Climb [fps]
 ceil = 10000; %Ceiling [ft]
-v_max = 180;  %Max Speed [mph]
-v_max = v_max*5280/3600; %Max speed [fps]
+v_max_sl = 150;  %Max Speed @ SL [mph]
+v_max_sl = v_max_sl*5280/3600; %Max speed @ SL [fps]
+v_max_10k = 180;  %Max Speed @ 10k [mph]
+v_max_10k = v_max_10k*5280/3600; %Max speed @ 10k [fps]
+v_stall = 80; %Stall speed @ 10k [mph]
+v_stall = v_stall*5280/3600; %Stall speed @ 10k [mph]
 W_payload = 20; %Weight of the payload
+W_max = 300;  %Max TO weight [lbs] 
+
+
 
 %Cruise Conditions - On our way to the fire
 L_D_cruise = 12;  %L/D Ratio for cruising [-]
@@ -45,23 +53,24 @@ W_fuel_desc_frac = 0.995;  %Descent fuel weight fraction [-]
 W_fuel_land_frac = 0.995;  %Landing fuel weight fraction [-]
 
 %Design Parameters (These should be changed as necessary)
-W_i = 100; %Initial Weight estimate [lbs]
+W_i = 50; %Initial Weight estimate [lbs]
 C_L = 1.2; %Coefficient of Lift guess [-]
 C_D0 = 0.030; %Parasitic drag coeff, typical value [-]
 
-outer_iter = 1000; 
-for k = 0:outer_iter
+outer_iter = 10000; 
+for k = 1:outer_iter
 
 %Random design parameter guesses
-b_w = 7*rand;  %Wingspan [ft]
-e = .7 + .3*rand; %Rectangular wing efficiency [-]
+b_w = 5 + 5*rand;  %Wingspan [ft]
+e = .7; %+ .3*rand; %Rectangular wing efficiency [-]
 lam_1_4 = 0; %Wing Quarter chord sweep [rad]
 lam = 1;   %Taper ratio [-]
-thicc = .15*rand;  %Maximum thickness ratio (Last 2 digits of NACA) [-]
-N = 1 + rand;  %Ultimate load factor [-]
-L_fuse = 2 + rand*3; %Length of fuselage [ft]
-Wid_fuse = 1 + rand;  %Width of fuselage [ft]
-D_fuse = 1 + rand; %Depth of fuselage [ft]
+thicc = .1 + .1*rand;  %Maximum thickness ratio (Last 2 digits of NACA) [-]
+LF = 1.2;  %Load Factor [-]
+N = LF*1.5;  %Ultimate load factor [-]
+L_fuse = 4 + rand*2; %Length of fuselage [ft]
+Wid_fuse = 1.5;  %Width of fuselage [ft]
+D_fuse = 1.5; %Depth of fuselage [ft]
 S_ht = 1 + 3*rand; %Horizontal tail surface area [ft^2]
 l_t = 1.5 + 3*rand; %Distance from wing 1/4 MAC to tail 1/4 MAC [ft]
 b_h = 1 + 2*rand; %Horizontal tail span [ft]
@@ -70,22 +79,27 @@ S_vt = 1 + 3*rand; %Vertical tail surface area [ft^2]
 b_v = 1 + 2*rand; %Vertical tail span [ft]
 t_VR = 3*rand; %Vertical tail max root thickness [in]
 
+if(l_t > L_fuse) %Check to see if l_t is greater - not possible
+    continue; %Then continue to next iteration
+end
+
 %Iteration stuff
 i = 0;
-max_iter = 50;
+max_iter = 50; %Convergence Iterations
 W_thresh = 10;  %Threshold of weight difference for convergence [lbs]
 
 
 while(i < max_iter)
     %Wing Area Calculations
-    S_w = 2*W_i/(rho_10k*C_L*v_max^2);  %Wing Surface Area [ft^2]
+    S_w = 2*W_i/(rho_10k*C_L*v_stall^2);  %Wing Surface Area [ft^2]
    
     %Drag calculations
-    A = b_w^2/S_w; 
+    A = b_w^2/S_w; % Aspect Ratio
     K = 1/(pi*A*e);
-    v = linspace(0,264);
-    D_10k = .5*rho_10k*v.^2*S_w*C_D0 + 2*K*W_i./(rho_10k*v.^2*S_w); %Drag Polar [lbf]
-    D_sl = .5*rho_sl*v.^2*S_w*C_D0 + 2*K*W_i./(rho_sl*v.^2*S_w); %Drag Polar [lbf]
+    v_sl = linspace(0,v_max_sl); % Velocity vector at sea level [fps]
+    v_10k = linspace(v_stall, v_max_10k);  %Velocity vector at 10k [fps]
+    D_10k = .5*rho_10k*v_10k.^2*S_w*C_D0 + 2*K*W_i./(rho_10k*v_10k.^2*S_w); %Drag Polar [lbf]
+    D_sl = .5*rho_sl*v_sl.^2*S_w*C_D0 + 2*K*W_i./(rho_sl*v_sl.^2*S_w); %Drag Polar [lbf]
     
     %Fuel Calculations
     W_fuel_cruise_frac = 1/(exp(R_cruise*c_p_cruise*...
@@ -98,9 +112,9 @@ while(i < max_iter)
     
     %Structure Weight Calculations (Nicolai Eqs)
     W_wing = 96.948*((W_i^N/10^5)^0.65*(A/cos(lam_1_4))^.57*(S_w/100)^.61*...
-        ((1+lam)/(2*thicc))^.36*(1+(v_max*.592))^.5)^.993; %Weight of the wing [lbs]
+        ((1+lam)/(2*thicc))^.36*(1+(v_max_sl*.592)/500)^.5)^.993; %Weight of the wing [lbs]
     W_fuse = 200*((W_i^N/10^5)^.286*(L_fuse/10)^.857*...
-        ((Wid_fuse+D_fuse)/10)*(v_max*.592/100)^.338)^1.1;  %Weight of the fuselage [lbs]
+        ((Wid_fuse+D_fuse)/10)*(v_max_sl*.592/100)^.338)^1.1;  %Weight of the fuselage [lbs]
     W_htail = 127*((W_i^N/10^5)^.87*(S_ht/100)^1.2*...
         (l_t/10)^.483*(b_h/t_HR)^.5)^.458;  %Weight of Horizontal tail [lbs]
     W_vtail = 98.5*((W_i^N/10^5)^.87*(S_vt/100)^1.2*...
@@ -110,12 +124,12 @@ while(i < max_iter)
     P_ex = RC*W_i; %Excess power [ft*lbs/s]
     P_ex = P_ex/550; %Excess power [hp]
     
-    P_req_10k = D_10k.*v; %Power required @ 10k [ft*lbs/s]
+    P_req_10k = D_10k.*v_10k; %Power required @ 10k [ft*lbs/s]
     P_req_10k = P_req_10k/550;  %Power required @ 10k[hp]
     P_av_10k = P_ex + P_req_10k;  %Power required @ 10k [hp]
     P_engine_10k = max(P_av_10k)/eta_pr_loit; %Power the engine needs to produce @ 10k [hp]
     
-    P_req_sl = D_sl.*v; %Power required @ SL [ft*lbs/s]
+    P_req_sl = D_sl.*v_sl; %Power required @ SL [ft*lbs/s]
     P_req_sl = P_req_sl/550;  %Power required @ SL [hp]
     P_av_sl = P_ex + P_req_sl;  %Power required @ SL [hp]
     P_engine_sl = max(P_av_sl)/eta_pr_loit; %Power the engine needs to produce @ SL [hp]
@@ -140,18 +154,20 @@ while(i < max_iter)
     W_eng_tot = 2.575*(W_engine)^.922; %Total Propulsion sys weight [lbs]
     W_fuelsys = 2.49*((Fuel_vol)^.6*(1/2)^.3*(1^.2)*(1^.13))^1.21;  %Fuel system weight [lbs]
     W_contsys= 1.08*W_i^.7;  %Control sys weight [lbs]
+    W_landgear = 5; %Landing gear weight [lbs]
     
     W_tot = W_payload + W_fuel + W_wing + W_fuse + W_htail +...
-        W_vtail + W_eng_tot + W_fuelsys + W_contsys;  %Total aircraft weight [lbs]
+        W_vtail + W_eng_tot + W_fuelsys + W_contsys + W_landgear;  %Total aircraft weight [lbs]
     
     %Check the weight to the compared weight
     if(abs(W_i - W_tot) < W_thresh)
-        
+        if (W_tot <= W_max) %Checl to make sure its less than max weight
         design_num = design_num + 1;
         %If good, save the design in the struct array
         Good_designs(design_num).weight = W_tot;
         Good_designs(design_num).S_w = S_w;
         Good_designs(design_num).b_w = b_w;  %Wingspan [ft]
+        Good_designs(design_num).A = A;    %Aspect Ratio [-]
         Good_designs(design_num).e = e; %Rectangular wing efficiency [-]
         Good_designs(design_num).lam_1_4 = lam_1_4; %Wing Quarter chord sweep [rad]
         Good_designs(design_num).lam = lam;   %Taper ratio [-]
@@ -169,9 +185,13 @@ while(i < max_iter)
         Good_designs(design_num).t_VR = t_VR; %Vertical tail max root thickness [in]
         Good_designs(design_num).eng_ind = index;  %Engine index
         Good_designs(design_num).eng_hp = engines(index,1);  %Engine power [hp]
+        Good_designs(design_num).W_S = W_tot/S_w;    %Wing Loading [lbs/ft^2]
+        Good_designs(design_num).Preq_W = P_needed/W_tot;  %Power Loading [hp/lb]  TODO Fix units
         
-        %Increase iteration index to break out
-        i = max_iter;
+        %Break out
+        break;
+        end % if (W_tot <= W_max)
+        
     else
         W_i = W_tot;
         i = i + 1;

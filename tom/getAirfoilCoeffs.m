e07d1a9a53d542db45b52b0fc1ac72e1e36a1472
tkantner <32119277+tkantner@users.noreply.github.com>
2019-03-20 16:13:35 -0700

    Update getAirfoilCoeffs.m

--- tom/getAirfoilCoeffs.m
+++ tom/getAirfoilCoeffs.m
@@ -3,10 +3,21 @@ function CD_airfoil = getAirfoilCoeffs(af_num, alpha, airfoil_drag)
     
     for i = 1:length(CD_airfoil) %Loop through coeff. vector
         for j = 1:size(airfoil_drag,1) %Loop through sheet
-            if (alpha < airfoil_drag(1, j)*pi/180) %Find the right alpha
+            if (alpha(i) < airfoil_drag(j, 1)*pi/180) %Find the right alpha
                 break;
             end
         end
-        CD_airfoil(i) = airfoil_drag(af_num + 1, j); %Add it to the vector
+
+        CD_airfoil(i) = airfoil_drag(j, af_num + 1);
+    end
+    
+    %Fix if the AOA is too high for lower speeds at SL
+    k = 1;
+    while(isnan(CD_airfoil(k)))
+        k = k + 1;
+    end
+    
+    for l = 1:(k - 1)
+        CD_airfoil(l) = CD_airfoil(k);
     end
 end
